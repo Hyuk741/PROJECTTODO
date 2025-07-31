a81ffb81208844f82328a1e95d4315b425625034
Hyuk741 <dnwlslzx12@gmail.com>
2025-07-31 18:55:07 +0900

    chapter_06과제댓글기능추가

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,15 +1,22 @@
-# todo > admin.py
-
 from django.contrib import admin
-from myapp.models import Todo
+
+from myapp.models import Todo, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    fields = ('message', 'user')
+
 
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'is_completed', 'start_date', 'end_date')
+    list_display = ('id', 'user', 'title', 'description', 'is_completed', 'start_date', 'end_date')
     list_filter = ('is_completed',)
     search_fields = ('title',)
     ordering = ('start_date',)
+    list_display_links = ('title',)
     fieldsets = (
         ('Todo Info', {
             'fields': ('title', 'description', 'is_completed')
@@ -18,7 +25,20 @@ class TodoAdmin(admin.ModelAdmin):
             'fields': ('start_date', 'end_date')
         }),
     )
+    inlines = [CommentInline]
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'todo', 'user', 'message', 'created_at')
+    list_filter = ('todo', 'user')
+    search_fields = ('message', 'user')
+    ordering = ('-created_at',)
+    list_display_links = ('message',)
+    fieldsets = (
+        ('Comment Info', {
+            'fields':('todo','user','message')
+        }),
+    )
 from django.contrib import admin
 
 # Register your models here.
--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from django.urls import reverse
+
 User = get_user_model()
 
 class Todo(models.Model):
@@ -13,9 +15,15 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField( auto_now=True)
 
+class Comment(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f'{self.user}: {self.message}'
 
 
 

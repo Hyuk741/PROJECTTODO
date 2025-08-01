cf4eac94317054f27e87aefca62367029fe7abff
Hyuk741 <dnwlslzx12@gmail.com>
2025-08-01 17:05:19 +0900

    chapter_07과제업로드,이미지테이블,썸네일추가

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,17 +1,32 @@
 from django import forms
 from myapp.models import Todo, Comment
+from django_summernote.widgets import SummernoteWidget
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["title", "description", "start_date", "end_date"]
-
+        widgets = {
+            'description': SummernoteWidget(),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '제목을 입력해주세요.'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+        }
+        
 class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["title", "description", "start_date", "end_date", "is_completed"]
-
+        widgets = {
+            'description': SummernoteWidget(),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '제목을 입력해주세요.'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'is_completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'completed_image': forms.FileInput(attrs={'class': 'form-control'})
+        }
+        
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -24,3 +39,5 @@ class CommentForm(forms.ModelForm):
                 'row': 3, 'cols': 40, 'class': 'form-control', 'placeholder' : '댓글 내용을 입력해주세요.'
             }),
         }
+
+        
